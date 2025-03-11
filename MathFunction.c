940e4c9bcf68f2ea14a9ab77906f081940d9086b
TheMakerMahes <umamaheswaran46@gmail.com>
2025-03-11 14:32:24 +0000

    CHORE : Math Function  in C

---
+++ MathFunction.c
@@ -0,0 +1,17 @@
+/*
+Mathematical Function in C programming Language
+*/
+#include<stdio.h>
+int main()
+{
+    printf("\nSquare root : %.2f",sqrt(4));
+    printf("\nPOW : %.2f",pow(2,3));
+    printf("\nabs : %d",abs(-4));
+    printf("\nceil : %.2f",ceil(10.8));
+    printf("\nceil : %.2f",ceil(10.4));
+    printf("\nfloor : %.2f",floor(10.8));
+    printf("\nfloor : %.2f",floor(10.4));
+    printf("\nround : %.2f",round(10.4));
+    printf("\nround : %.2f",round(10.6));
+    return 0;
+}
