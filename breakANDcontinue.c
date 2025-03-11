db1a10927c52f75a32befc70db4c34167c8625a0
TheMakerMahes <umamaheswaran46@gmail.com>
2025-03-11 14:26:10 +0000

    CHORE : sample break statement.c

---
+++ breakANDcontinue.c
@@ -0,0 +1,13 @@
+//Break & Continue Statement
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        if(i==7)
+            break;
+        printf("\n%d",i);
+    }
+    return 0;
+}
