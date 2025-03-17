e6803019625aa6f75f9348fcda44e979e4fab9c3
TheMakerMahes <umamaheswaran46@gmail.com>
2025-03-17 17:22:09 +0000

    CHORE : sample

---
+++ enum.c
@@ -0,0 +1,20 @@
+//enumerations or enum in c programming
+#include<stdio.h>
+ enum mahes{no,yes=20};
+
+ int main()
+ {
+
+     enum maddy {x=10,y=20};
+     enum mahes a;
+     enum maddy b;
+     enum mr {d=10,f};
+     enum mr c;
+     a=no;
+     b=y;
+     c=f;
+     printf("\nA : %d",a);
+     printf("\nB : %d",b);
+     printf("\nC : %d",c);
+
+ }
